b9b956e06c80bb472ce5a6213a1e86afe6db3565
ios lab <ios@lab.com>
2025-08-09 12:58:37 -0300

    Aula 9 ago

--- ios-lab-phone/ContentView.swift
+++ ios-lab-phone/ContentView.swift
@@ -1,24 +1,60 @@
-//
-//  ContentView.swift
-//  ios-lab-phone
-//
-//  Created by iOS Lab on 09/08/25.
-//
+// Desafio 5
+
+/**
+ */
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Desafio5(
+            name: "Filipe",
+            systemName: "chevron.right"
+        )
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct Desafio5 : View {
+    
+    let name: String
+    let subtitle: String
+    let systemName: String
+    
+    init(
+        name: String = "Seu nome",
+        subtitle: String = "Subtitulo de sua rescolha",
+        systemName: String = "chevron.right"
+    ) {
+        self.name = name
+        self.subtitle = subtitle
+        self.systemName = systemName
+    }
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                HStack{
+                    VStack(
+                        alignment: .leading
+                    ){
+                        Text(name)
+                        Text(subtitle)
+                    }
+                    Spacer()
+                    Image(systemName: systemName)
+                        .foregroundColor(.blue)
+                        .bold()
+                }
+                .padding()
+            }
+            .background(.gray.opacity(0.3))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+        .frame(height: 100)
+        .padding()
+    }
 }
