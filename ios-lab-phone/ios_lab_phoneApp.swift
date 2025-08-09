e66a896afe1a04e7d5e1c45ecae100b215208701
ios lab <ios@lab.com>
2025-08-09 12:11:38 -0300

    Initial Commit

---
+++ ios-lab-phone/ios_lab_phoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_lab_phoneApp.swift
+//  ios-lab-phone
+//
+//  Created by iOS Lab on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_lab_phoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
